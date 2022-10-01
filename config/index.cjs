b0f40b1973d4f95763d227337a7773432ef7c664
zzlX5 <114820273+zzlX5@users.noreply.github.com>
2022-10-01 23:23:56 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,56 +8,54 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wxc353c65af6c4a554',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '5c44b5de9c37ccf9f3f6a85edc42056a',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '内蒙古',
+  CITY: '呼和浩特',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '吴雪妍宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: [ocI2d5-ZGPp3mnR9LvJuMs0Zhf70,ocI2d5zENxSaXozfmq1BWV9W2_rM],
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'Y62J0El5IMBsKIdIv1P1WVzP1--bK6HXW22JsFfgLKg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '03-09',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '吴雪妍宝贝', year: '2003', date: '02-07',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '吴雪妍宝贝', year: '2003', date: '03-09',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2022', date: '01-16',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'love_day', date: '2022-05-23' },
       ],
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'Y62J0El5IMBsKIdIv1P1WVzP1--bK6HXW22JsFfgLKg',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: [ocI2d5-ZGPp3mnR9LvJuMs0Zhf70,ocI2d5zENxSaXozfmq1BWV9W2_rM],
     }
   ],
 
